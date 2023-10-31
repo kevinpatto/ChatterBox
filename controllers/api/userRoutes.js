d0c1732bf441b4f37a6b7af854a76c7f97b31cd7
Jackson Neuman <jacksonneuman@Jacksons-Air-4.attlocal.net>
2023-10-31 11:43:52 -0500

    pulling

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,10 @@
+const router = require('express').Router();
+const { User, Chatroom } = require('../../models');
+
 // /login
 // /signup
 // /chatrooms
 // /chat
 // /{profilename}
+
+module.exports = router;
