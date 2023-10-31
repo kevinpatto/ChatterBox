3c1c5995aff2d8f5e8dbdfc0765baa892630ba56
Kevin Patto <kevp2010@gmail.com>
2023-10-31 11:32:09 -0500

    added get route to test if server is working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,4 +3,12 @@ const { Chatroom, User } = require('../models');
 
 // ROUTES NEEDED: TBD
 
+router.get('/', async (req, res) => {
+	try {
+	  res.send('hello world');
+	} catch (err) {
+	  res.status(500).json(err);
+	}
+  });
+
 module.exports = router;
