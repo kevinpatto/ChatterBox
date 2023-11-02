a33beb4c4e61d92ecd66b2dd8c5435abd9ad8c82
Kevin Patto <kevp2010@gmail.com>
2023-11-02 18:40:07 -0500

    changed name to username

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
-	checkPassword(loginPw) {
+	async checkPassword(loginPw) {
 		return bcrypt.compareSync(loginPw, this.password);
 	}
 }
@@ -16,7 +16,7 @@ User.init(
 			primaryKey: true,
 			autoIncrement: true,
 		},
-		name: {
+		username: {
 			type: DataTypes.STRING,
 			allowNull: false,
 		},
