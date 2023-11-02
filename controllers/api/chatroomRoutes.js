5a5ce3e8be2c609392a3ca66e58ca90fddb22342
Jackson Neuman <jacksonneuman@Jacksons-Air-4.attlocal.net>
2023-11-02 11:32:17 -0500

    working on feature

--- controllers/api/chatroomRoutes.js
+++ controllers/api/chatroomRoutes.js
@@ -3,4 +3,25 @@ const { Chatroom } = require('../../models');
 
 // ROUTES NEEDED: TBD
 
+// Route to view chatrooms
+router.get('/chatrooms', async (req, res) => {
+    try {
+      const chatrooms = await Chatroom.findAll();
+      res.json(chatrooms);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // Route to join or interact with a specific chatroom
+router.get('/chat/:chatroomId', async (req, res) => {
+    const chatroomId = req.params.chatroomId;
+    try {
+      // Implement logic to join a specific chatroom or send messages
+  
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,27 +1,6 @@
 const router = require('express').Router();
-const { User, Chatroom } = require('../../models');
+const { User } = require('../../models');
 
-
-// Route to view chatrooms
-router.get('/chatrooms', async (req, res) => {
-  try {
-    const chatrooms = await Chatroom.findAll();
-    res.json(chatrooms);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Route to join or interact with a specific chatroom
-router.get('/chat/:chatroomId', async (req, res) => {
-  const chatroomId = req.params.chatroomId;
-  try {
-    // Implement logic to join a specific chatroom or send messages
-
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 // Route to view a user's profile
 router.get('/:profilename', async (req, res) => {
@@ -40,8 +19,8 @@ router.post('/login', async (req, res) => {
   try {
     // Implement user login logic here
     // Verify credentials and create a user session
-    const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const { name, password } = req.body;
+    const user = await User.findOne({ where: { name } });
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
@@ -69,16 +48,15 @@ router.post('/signup', async (req, res) => {
         // Implement user signup logic here
         // Create a new user in the database
         // Extract user information from the request body
-        const { name, email, password } = req.body;
+        const { name, password } = req.body;
         // Check if the email is already in use
-        const existingUser = await User.findOne({ where: { email } });
+        const existingUser = await User.findOne({ where: { name } });
         if (existingUser) {
-        return res.status(400).json({ message: 'Email already in use' });
+        return res.status(400).json({ message: 'Name already in use' });
         }
 
         const newUser = await User.create({
             name,
-            email,
             password,
         });
         res.status(201).json({ message: 'User registered successfully', user: newUser });
