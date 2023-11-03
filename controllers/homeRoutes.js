3c8d82d36694c33ae6dbce28886230c32ce7f583
Kevin Patto <kevp2010@gmail.com>
2023-11-03 10:01:04 -0500

    changed / route to render homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,9 +5,8 @@ const { Chatroom, User } = require('../models');
 // ROUTES NEEDED: TBD
 
 router.get('/', async (req, res) => {
-	// for now, it will just render the same page as /chat. in the future, it will render homepage.handlebars
 	try {
-		res.render('chat', { logged_in: req.session.logged_in, username: req.session.username });
+		res.render('homepage', { logged_in: req.session.logged_in, username: req.session.username });
 	} catch (err) {
 		res.status(500).json(err);
 	}
