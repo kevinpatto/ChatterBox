ae60a076f7d6343149c3a83f9f227929b68df3a6
Kevin Patto <kevp2010@gmail.com>
2023-11-03 10:18:49 -0500

    /chat endpoint no longer needed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,15 +6,33 @@ const { Chatroom, User } = require('../models');
 
 router.get('/', async (req, res) => {
 	try {
-		res.render('homepage', { logged_in: req.session.logged_in, username: req.session.username });
+		const chatroomsData = await Chatroom.findAll();
+
+		// serialize for handlebars
+		const chatrooms = chatroomsData.map((chatroom) => chatroom.get({ plain: true }));
+
+		res.render('homepage', { logged_in: req.session.logged_in, username: req.session.username, chatrooms });
 	} catch (err) {
 		res.status(500).json(err);
 	}
 });
 
-router.get('/chat', async (req, res) => {
+// router.get('/chat', async (req, res) => {
+// 	try {
+// 		res.render('chat', { logged_in: req.session.logged_in, username: req.session.username });
+// 	} catch (err) {
+// 		res.status(500).json(err);
+// 	}
+// });
+
+router.get('/chat/:id', async (req, res) => {
 	try {
-		res.render('chat', { logged_in: req.session.logged_in, username: req.session.username });
+		const chatroomData = await Chatroom.findByPk(req.params.id, {});
+		console.log(chatroomData);
+
+		const chatroom = chatroomData.get({ plain: true });
+
+		res.render('chat', { logged_in: req.session.logged_in, username: req.session.username, chatroom });
 	} catch (err) {
 		res.status(500).json(err);
 	}
