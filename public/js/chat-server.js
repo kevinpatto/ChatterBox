40f1de0596705aad93a95d5ec9e6ad26a41621bd
cj cabrera <cabrera012500@gmail.com>
2023-11-02 21:55:45 -0500

    fixed logout

--- public/js/chat-server.js
+++ public/js/chat-server.js
@@ -1,27 +1,31 @@
-// chat.js (Server-Side JavaScript)
-const { v4: uuidv4 } = require('uuid');
+// DO NOT USE CURRENTLY 
+// EVERYTHING THAT NEEDS TO BE DONE 
+// SERVER SIDE IS IN SERVER.JS
 
-function generateUniqueRoomId() {
-    // Generate a unique room ID using UUID
-    return uuidv4();
-}
+// // chat.js (Server-Side JavaScript)
+// const { v4: uuidv4 } = require('uuid');
 
-function handleUserInitiatedChat(socket) {
-    // socket.on('start-chat', () => {
-    //     // Generate a unique room ID
-    //     const roomId = generateUniqueRoomId();
+// function generateUniqueRoomId() {
+//     // Generate a unique room ID using UUID
+//     return uuidv4();
+// }
+
+// function handleUserInitiatedChat(socket) {
+//     // socket.on('start-chat', () => {
+//     //     // Generate a unique room ID
+//     //     const roomId = generateUniqueRoomId();
     
-    //     // Create the room and notify the user to join it
-    //     socket.join(roomId);
-    //     socket.emit('room-created', roomId);
-    // });
+//     //     // Create the room and notify the user to join it
+//     //     socket.join(roomId);
+//     //     socket.emit('room-created', roomId);
+//     // });
 
-    // Handle sending and broadcasting messages
-    socket.on('send-message', (message) => {
-        console.log(`MessageReceived: ${message}`)
-        // Broadcast the message to everyone in the room
-        // socket.to(socket.roomId).emit('new-message', message);
-    });
-}
+//     // Handle sending and broadcasting messages
+//     socket.on('send-message', (message) => {
+//         console.log(`MessageReceived: ${message}`)
+//         // Broadcast the message to everyone in the room
+//         // socket.to(socket.roomId).emit('new-message', message);
+//     });
+// }
 
-module.exports = handleUserInitiatedChat;
+// module.exports = handleUserInitiatedChat;
