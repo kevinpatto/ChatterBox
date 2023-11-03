74f67c554fc6f2b68597113df200b416a28d8c18
cj cabrera <cabrera012500@gmail.com>
2023-11-03 12:46:23 -0500

    added to username pass

--- public/js/chat-client.js
+++ public/js/chat-client.js
@@ -3,6 +3,7 @@ const message = document.getElementById('message');
 const sendButton = document.getElementById('send-btn');
 const messagesDisplay = document.getElementById('messages');
 const roomID = document.getElementById('room-ID')
+const user = document.getElementById('username');
 
 // io.emit('join', roomID);
 
@@ -12,10 +13,11 @@ sendButton.addEventListener('click', async (event) => {
     event.preventDefault();
     console.log("clicked")
     const messageValue = message.value;
+    const userValue = user.value;
     if (messageValue) {
         console.log(':' + messageValue)
         // Emit a "send-message" event to the server
-        io.emit('message', messageValue);
+        io.emit('message', messageValue,userValue);
         // Clear the input field
         message.value = '';
     }
